8af7442fefad2e93783c409d2fd1d599c59fb5b9
alekarhis <alekarhis@pm.me>
2024-02-15 00:29:03 +0100

    test wrong user cannot delete blog

--- bloglist-frontend/cypress/e2e/bloglist-app.cy.js
+++ bloglist-frontend/cypress/e2e/bloglist-app.cy.js
@@ -79,64 +79,80 @@ describe('BLOG APP', function() {
       cy.get('@form').contains('label', /title/i).next('input').as('title')
       cy.get('@form').contains('label', /author/i).next('input').as('author')
       cy.get('@form').contains('label', /url/i).next('input').as('url')
-			
-			cy.get('@title').type(testBlog.title)
-			cy.get('@author').type(testBlog.author)
-			cy.get('@url').type(testBlog.url)
-			cy.get('button[type="submit"]').click()
 
-			cy.get('a:contains("Go to statement considered harmful")').should('exist')
+      cy.get('@title').type(testBlog.title)
+      cy.get('@author').type(testBlog.author)
+      cy.get('@url').type(testBlog.url)
+      cy.get('button[type="submit"]').click()
+
+      cy.get('a:contains("Go to statement considered harmful")').should('exist')
     })
 
-		describe('BLOGLIST POPULATED', function() {
-			const blogs = [
-				{
-					title: 'Go to statement considered harmful',
-					author: 'Edsger W. Dijkstra',
-					url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Consider…'
-				},
-				{
-					title: "First class tests",
-					author: "Robert C. Martin",
-					url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-					likes: 8
-				},
-				{
-					title: "TDD harms architecture",
-					author: "Robert C. Martin",
-					url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-					likes: 5
-				},
-				{
-					title: "Type wars",
-					author: "Robert C. Martin",
-					url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-					likes: 3
-				}  
-			]			
+    describe('BLOGLIST POPULATED', function() {
+      const blogs = [
+        {
+          title: 'Go to statement considered harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Consider…'
+        },
+        {
+          title: 'First class tests',
+          author: 'Robert C. Martin',
+          url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+          likes: 8
+        },
+        {
+          title: 'TDD harms architecture',
+          author: 'Robert C. Martin',
+          url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+          likes: 5
+        },
+        {
+          title: 'Type wars',
+          author: 'Robert C. Martin',
+          url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+          likes: 3
+        }
+      ]
 
-			beforeEach(function() {
-				blogs.forEach(function(blog) {
-					cy.createBlog(blog)
+      beforeEach(function() {
+        blogs.forEach(function(blog) {
+          cy.createBlog(blog)
         })
-				cy.visit('http://localhost:5173')
-			})
-				
-			it('can like blog', function() {
-				cy.get('p:contains("TDD harms architecture")').parent().as('blog')
-				cy.get('@blog').find('button:contains("show")').click()
-				cy.get('@blog').find('button:contains("Like")').as('likeButton')
-				cy.get('@likeButton').click()
-				cy.get('@likeButton').parent().should('contain', '6')
-			})
+        cy.visit('http://localhost:5173')
+      })
 
-			it('can delete own blog', function() {
-				cy.get('p:contains("TDD harms architecture")').parent().as('blog')
-				cy.get('@blog').find('button:contains("show")').click()
-				cy.get('@blog').find('button:contains("Delete")').click()
-				cy.get('p:contains("TDD harms architecture")').should('not.exist')
-				//cy.get('p:contains("TDD harms architecture")').toBe(null)
-			})
-		})
+      it('can like blog', function() {
+        cy.get('p:contains("TDD harms architecture")').parent().as('blog')
+        cy.get('@blog').find('button:contains("show")').click()
+        cy.get('@blog').find('button:contains("Like")').as('likeButton')
+        cy.get('@likeButton').click()
+        cy.get('@likeButton').parent().should('contain', '6')
+      })
+
+      it('can delete own blog', function() {
+        cy.get('p:contains("TDD harms architecture")').parent().as('blog')
+        cy.get('@blog').find('button:contains("show")').click()
+        cy.get('@blog').find('button:contains("Delete")').click()
+        cy.get('p:contains("TDD harms architecture")').should('not.exist')
+        //cy.get('p:contains("TDD harms architecture")').toBe(null)
+      })
+
+      it.only('cannot delete other users blogs' , function() {
+        const unauthorizedUser = {
+          username: 'arnauserra',
+          password: 'peruna'
+        }
+        cy.request('POST', 'users', unauthorizedUser)
+        cy.request('POST', 'login', unauthorizedUser)
+          .then(function(res) {
+            localStorage.setItem('user', JSON.stringify(res.body))
+            cy.visit('http://localhost:5173')
+          })
+        cy.get('p:contains("TDD harms architecture")').parent().as('blog')
+        cy.get('@blog').find('button:contains("show")').click()
+        cy.get('@blog').find('button:contains("Delete")').should('not.exist')
+      })
+    })
   })
 })
